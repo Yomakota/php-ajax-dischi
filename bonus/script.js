e7301f4134646a49837c29d3e9c64a88a0b1bf62
Yomakota <89361386+Yomakota@users.noreply.github.com>
2022-07-27 06:45:59 +0200

    add dinamic select

--- bonus/script.js
+++ bonus/script.js
@@ -3,6 +3,7 @@ const App = new Vue(
         el: '#root',
         data: {
             albums: [],
+            genres: [],
             valueSelect: 'All',
             url: 'http://localhost:8888/php-ajax-dischi/bonus/apiGenre.php?genre=',
         },
@@ -15,7 +16,15 @@ const App = new Vue(
         },
         created() {
             axios.get(`${this.url}${'All'}`)
-                .then((result) => { this.albums = result.data.results; })
+                .then((result) => {
+                    this.albums = result.data.results;
+
+                    this.albums.forEach((element) => {
+                        if (!this.genres.includes(element.genre)) {
+                            this.genres.push(element.genre);
+                        }
+                    });
+                })
                 .catch((error) => { console.log(error); });
         }
     }
